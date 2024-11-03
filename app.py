0f5a323f80c7fd2e52cdb7199d24a85fedfa609f
Somendra Singh <71236256+somendrew@users.noreply.github.com>
2024-11-03 07:43:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import subprocess
-import sys
-
-def install(package):
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-install('opencv-python')
-
 import cv2
 import os
 import numpy as np
